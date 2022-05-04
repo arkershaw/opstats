ce9ac554f1a2c0ec880c85428062e021b298cd13
Andy Kershaw <arkershaw@users.noreply.github.com>
2022-05-04 23:38:02 +0100

    Better error handling. Add flag for pop/sample variance.

--- opstats.py
+++ opstats.py
@@ -3,6 +3,10 @@ from typing import NamedTuple, List, Union
 from dataclasses import dataclass
 
 
+class InvalidStateException(Exception):
+    pass
+
+
 class Stats(NamedTuple):
     """
     Results of moment calculations.
@@ -14,30 +18,40 @@ class Stats(NamedTuple):
     mean: float
         the mean value of all data points
     variance: float
-        the calculated sample variance
+        the calculated variance (population or sample depending on parameters)
     standard_deviation: float
         the standard deviation (sqrt(variance)) for convenience
     skew: float
         the skewness (Fisher-Pearson)
     kurtosis: float
-        the kurtosis (Pearson) 
+        the kurtosis (Pearson)
     """
 
-    count: int
+    sample_count: int
     mean: float
     variance: float
     standard_deviation: float
-    skew: float
+    skewness: float
     kurtosis: float
 
 
-# Translated from https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+# Adapted from https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
 class OnlineCalculator:
     """
-    Online algorithm for calculating mean, sample variance, skewness and kurtosis.
+    Online algorithm for calculating mean, variance, skewness and kurtosis.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, sample_variance: bool = False) -> None:
+        """
+        Initialise a new calculator.
+
+        Parameters
+        ----------
+        sample_variance: bool, optional
+            set to True to calculate the sample varaiance instead of the population variance
+        """
+
+        self._sample_var = sample_variance
         self._n = 0
         self._mean = 0
         self._M2 = 0
@@ -61,7 +75,7 @@ class OnlineCalculator:
         delta_n2 = delta_n * delta_n
         term1 = delta * delta_n * n1
         self._mean = self._mean + delta_n
-        self._M4 = self._M4 + term1 * delta_n2 * (self._n*self._n - 3*self._n + 3) + 6 * delta_n2 * self._M2 - 4 * delta_n * self._M3
+        self._M4 = self._M4 + term1 * delta_n2 * (self._n * self._n - 3 * self._n + 3) + 6 * delta_n2 * self._M2 - 4 * delta_n * self._M3
         self._M3 = self._M3 + term1 * delta_n * (self._n - 2) - 3 * delta_n * self._M2
         self._M2 = self._M2 + term1
 
@@ -72,23 +86,35 @@ class OnlineCalculator:
         Returns
         -------
         Stats
-            Named tuple containing the calculated statistics
+            named tuple containing the calculated statistics
+
+        Raises
+        ------
+        InvalidStateException
+            less than two data points have been added
         """
 
-        # TODO: Add option for population variance.
-        variance = self._M2 / (self._n - 1)
-        skew = (sqrt(self._n) * self._M3) / (self._M2 ** (3/2))
+        if self._n < 2:
+            raise InvalidStateException('At least two data points must be added.')
+
+        if self._sample_var:
+            var = self._M2 / (self._n - 1)
+        else:
+            var = self._M2 / self._n
+
         # If all the inputs are the same, M2 will be 0, resulting in a division by 0.
         if self._M2 > 0:
-            kurtosis = (self._n * self._M4) / (self._M2 * self._M2) - 3
+            skew = (sqrt(self._n) * self._M3) / (self._M2 ** (3 / 2))
+            kurt = (self._n * self._M4) / (self._M2 * self._M2) - 3
         else:
-            kurtosis = 0.0
+            skew = 0.0
+            kurt = -3.0
 
-        return Stats(self._n, self._mean, variance, sqrt(variance), skew, kurtosis)
+        return Stats(self._n, self._mean, var, sqrt(var), skew, kurt)
 
 
 # Translated from https://rdrr.io/cran/utilities/src/R/sample.decomp.R
-def aggregate_stats(stats: List[Stats]) -> Stats:
+def aggregate_stats(stats: List[Stats], sample_variance: bool = False) -> Stats:
     """
     Combines a list of Stats tuples previously calculated in parallel.
 
@@ -96,6 +122,8 @@ def aggregate_stats(stats: List[Stats]) -> Stats:
     ----------
     stats: List[Stats]
         list of separate instances of calculated statistics from one data set
+    sample_variance: bool, optional
+        population variance is calculated by default. Set to True to calculate the sample varaiance
 
     Returns
     -------
@@ -122,9 +150,12 @@ def aggregate_stats(stats: List[Stats]) -> Stats:
     sum_mean = 0.0
     sum_ss = 0.0
     for sample in stats:
-        pool.n += sample.count
-        sum_mean += (sample.mean * sample.count)
-        _ss = (sample.count - 1) * sample.variance
+        pool.n += sample.sample_count
+        sum_mean += (sample.mean * sample.sample_count)
+        if sample_variance:
+            _ss = (sample.sample_count - 1) * sample.variance
+        else:
+            _ss = sample.sample_count * sample.variance
         SS.append(_ss)
         sum_ss += _ss
 
@@ -141,32 +172,34 @@ def aggregate_stats(stats: List[Stats]) -> Stats:
         sample = stats[i]
         _dev = sample.mean - pool.mean
         deviation.append(_dev)
-        sum_n_dev_2 += sample.count * _dev ** 2
+        sum_n_dev_2 += sample.sample_count * _dev ** 2
         sum_ss_dev += SS[i] * _dev
-        sum_n_dev_3 += sample.count * _dev ** 3
+        sum_n_dev_3 += sample.sample_count * _dev ** 3
         sum_ss_dev_2 += SS[i] * _dev ** 2
-        sum_n_dev_4 += sample.count * _dev ** 4
+        sum_n_dev_4 += sample.sample_count * _dev ** 4
 
     pool.SS = sum_ss + sum_n_dev_2
-    pool.var = pool.SS / (pool.n - 1)
+    if sample_variance:
+        pool.var = pool.SS / (pool.n - 1)
+    else:
+        pool.var = pool.SS / pool.n
     pool.sd = sqrt(pool.var)
 
     # Third pass - calculate the skew and kurtosis.
-    # TODO: Implement alternative adjustments.
+    # TODO: Implement Adjusted Fisher Pearson.
     def skew_adj(n: float) -> float:
         return 1
-        return ((n-1)/n)**(3/2)  # 'Moment', 'b', 'Minitab'
-        return sqrt(n*(n-1))/(n-2)  # 'Adjusted Fisher Pearson', 'G', 'Excel', 'SPSS', 'SAS'
+        return ((n - 1) / n) ** (3 / 2)  # 'Moment', 'b', 'Minitab'
+        return sqrt(n * (n - 1)) / (n - 2)  # 'Adjusted Fisher Pearson', 'G', 'Excel', 'SPSS', 'SAS'
 
     def kurt_adj(n: float) -> float:
         return 1
-        return ((n-1)/n)**2  # 'Moment', 'b', 'Minitab'
-        return (n+1)*(n-1)/((n-2)*(n-3))  # 'Adjusted Fisher Pearson', 'G', 'Excel', 'SPSS', 'SAS'
+        return ((n - 1) / n) ** 2  # 'Moment', 'b', 'Minitab'
+        return (n + 1) * (n - 1) / ((n - 2) * (n - 3))  # 'Adjusted Fisher Pearson', 'G', 'Excel', 'SPSS', 'SAS'
 
     def excess_adj(n: float) -> float:
-        # return 0
         return -3  # Excess
-        return -3*(n-1)**2/((n-2)*(n-3))  # Excess 'Adjusted Fisher Pearson', 'G', 'Excel', 'SPSS', 'SAS
+        return -3 * (n - 1) ** 2 / ((n - 2) * (n - 3))  # Excess 'Adjusted Fisher Pearson', 'G', 'Excel', 'SPSS', 'SAS
 
     SC = []
     SQ = []
@@ -175,18 +208,18 @@ def aggregate_stats(stats: List[Stats]) -> Stats:
     sum_sc_dev = 0.0
     for i in range(len(stats)):
         sample = stats[i]
-        _sc = sample.skew * (SS[i]**(3/2))/(skew_adj(sample.count)*sqrt(sample.count))
+        _sc = sample.skewness * (SS[i] ** (3 / 2)) / (skew_adj(sample.sample_count) * sqrt(sample.sample_count))
         SC.append(_sc)
         sum_sc += _sc
-        _sq = (sample.kurtosis - excess_adj(sample.count))*SS[i]**2/(kurt_adj(sample.count)*sample.count)
+        _sq = (sample.kurtosis - excess_adj(sample.sample_count)) * SS[i] ** 2 / (kurt_adj(sample.sample_count) * sample.sample_count)
         SQ.append(_sq)
         sum_sq += _sq
         sum_sc_dev += _sc * deviation[i]
 
     pool.SC = sum_sc + 3 * sum_ss_dev + sum_n_dev_3
-    pool.skew = skew_adj(pool.n)*sqrt(pool.n)*pool.SC/pool.SS**(3/2)
-    pool.SQ = sum_sq + 4*sum_sc_dev + 6*sum_ss_dev_2 + sum_n_dev_4
-    pool.kurt = kurt_adj(pool.n)*pool.n*pool.SQ/pool.SS**2 + excess_adj(pool.n)
+    pool.skew = skew_adj(pool.n) * sqrt(pool.n) * pool.SC / pool.SS ** (3 / 2)
+    pool.SQ = sum_sq + 4 * sum_sc_dev + 6 * sum_ss_dev_2 + sum_n_dev_4
+    pool.kurt = kurt_adj(pool.n) * pool.n * pool.SQ / pool.SS ** 2 + excess_adj(pool.n)
 
     return Stats(
         pool.n,
--- tests/test_opstats.py
+++ tests/test_opstats.py
@@ -1,86 +1,126 @@
-from math import sqrt
+from typing import List, Union
 import unittest
 
-from opstats import Stats, OnlineCalculator, aggregate_stats
+import numpy
+import scipy.stats
 
+from opstats import Stats, OnlineCalculator, aggregate_stats, InvalidStateException
 
-SAMPLE_DATA = [
-    25, 97, 76, 94, 49, 85, 65, 77, 22, 13,
-    55, 99, 90, 59, 95, 79, 38, 2, 83, 60,
-    21, 14, 52, 44, 48, 84, 51, 82, 72, 30,
-    93, 54, 64, 89, 88, 35, 67, 28, 46, 74,
-    78, 68, 16, 53, 10, 69, 66, 34, 31, 18
-]
+# Use 101 elements to get uneven sample sizes when dividing into three lists.
+RANDOM_INTS = list(numpy.random.randint(1, 100, 101))
+RANDOM_FLOATS = list(numpy.random.rand(101))
 
 
 class TestOnlineCalculator(unittest.TestCase):
     def compare_stats(self, left: Stats, right: Stats) -> None:
-        self.assertEqual(len(SAMPLE_DATA), left.count)
-        self.assertEqual(len(SAMPLE_DATA), right.count)
+        self.assertEqual(left.sample_count, right.sample_count)
         self.assertAlmostEqual(left.mean, right.mean)
         self.assertAlmostEqual(left.variance, right.variance)
         self.assertAlmostEqual(left.standard_deviation, right.standard_deviation)
-        self.assertAlmostEqual(left.skew, right.skew)
+        self.assertAlmostEqual(left.skewness, right.skewness)
         self.assertAlmostEqual(left.kurtosis, right.kurtosis)
 
-    def write_stats(self, label: str, stats: Stats) -> None:
-        print(f'{label} count: {stats.mean}')
-        print(f'{label} mean: {stats.mean}')
-        print(f'{label} sample variance: {stats.variance}')
-        print(f'{label} standard deviation: {stats.standard_deviation}')
-        print(f'{label} skew: {stats.skew}')
-        print(f'{label} kurtosis: {stats.kurtosis}')
-    
-    def calculate_scipy(self) -> Stats:
-        import scipy.stats
+    def calculate_scipy(self, data_points: Union[List[int], List[float]], sample_variance: bool = False) -> Stats:
+        count = len(data_points)
+        mean = numpy.mean(data_points)
+        if sample_variance:
+            var = numpy.var(data_points, ddof=1)
+            sd = numpy.std(data_points, ddof=1)
+        else:
+            var = numpy.var(data_points)
+            sd = numpy.std(data_points)
+        skew = scipy.stats.skew(data_points)
+        kurt = scipy.stats.kurtosis(data_points)
 
-        count = len(SAMPLE_DATA)
-        mean = scipy.stats.tmean(SAMPLE_DATA)
-        s_var = scipy.stats.tvar(SAMPLE_DATA)
-        sd = scipy.stats.tstd(SAMPLE_DATA)
-        skew = scipy.stats.skew(SAMPLE_DATA)
-        kurt = scipy.stats.kurtosis(SAMPLE_DATA)
+        return Stats(count, mean, var, sd, skew, kurt)
 
-        return Stats(count, mean, s_var, sd, skew, kurt)
-
-    def calculate(self) -> Stats:
-        calculator = OnlineCalculator()
-        for data_point in SAMPLE_DATA:
+    def calculate(self, data_points: Union[List[int], List[float]], sample_variance: bool = False) -> Stats:
+        calculator = OnlineCalculator(sample_variance=sample_variance)
+        for data_point in data_points:
             calculator.add(data_point)
         return calculator.get()
 
-    def test_statistics(self) -> None:
-        import statistics
+    def calculate_parallel(self, data_points: Union[List[int], List[float]], sample_variance: bool = False) -> Stats:
+        # Split the list into three to get uneven sample sizes.
+        size = len(data_points) // 3
 
-        mean = statistics.mean(SAMPLE_DATA)
-        s_var = statistics.variance(SAMPLE_DATA)
-        sd = sqrt(s_var)
-        # Skew and kurtosis not available.
+        first_data = data_points[:size]
+        first_calc = OnlineCalculator(sample_variance=sample_variance)
+        for d in first_data:
+            first_calc.add(d)
 
-        result = self.calculate()
+        second_data = data_points[size:size * 2]
+        second_calc = OnlineCalculator(sample_variance=sample_variance)
+        for d in second_data:
+            second_calc.add(d)
 
-        self.assertEqual(len(SAMPLE_DATA), result.count)
-        self.assertAlmostEqual(mean, result.mean)
-        self.assertAlmostEqual(s_var, result.variance)
-        self.assertAlmostEqual(sd, result.standard_deviation)
-        self.assertAlmostEqual(mean, result.mean)
+        third_data = data_points[size * 2:]
+        third_calc = OnlineCalculator(sample_variance=sample_variance)
+        for d in third_data:
+            third_calc.add(d)
 
-    def test_scipy(self) -> None:
-        scipy_result = self.calculate_scipy()
-        result = self.calculate()
+        return aggregate_stats([first_calc.get(), second_calc.get(), third_calc.get()], sample_variance=sample_variance)
+
+    def test_empty(self) -> None:
+        data_points = []
+        with self.assertRaisesRegex(InvalidStateException, 'At least two data points must be added.'):
+            self.calculate(data_points)
+
+    def test_single_value(self) -> None:
+        data_points = [1.0]
+        with self.assertRaisesRegex(InvalidStateException, 'At least two data points must be added.'):
+            self.calculate(data_points)
+
+    def test_zeros(self) -> None:
+        data_points = [0.0, 0.0]
+        scipy_result = self.calculate_scipy(data_points)
+        result = self.calculate(data_points)
         self.compare_stats(scipy_result, result)
 
-    def test_aggregate_stats(self) -> None:
-        scipy_result = self.calculate_scipy()
+    def test_ones(self) -> None:
+        data_points = [1.0, 1.0, 1.0]
+        scipy_result = self.calculate_scipy(data_points)
+        result = self.calculate(data_points)
+        self.compare_stats(scipy_result, result)
 
-        left_data = SAMPLE_DATA[:len(SAMPLE_DATA)//2]
-        right_data = SAMPLE_DATA[len(SAMPLE_DATA)//2:]
-        left = OnlineCalculator()
-        for d in left_data:
-            left.add(d)
-        right = OnlineCalculator()
-        for d in right_data:
-            right.add(d)
-        result = aggregate_stats([left.get(), right.get()])
+    def test_sample_variance_integer(self) -> None:
+        scipy_result = self.calculate_scipy(RANDOM_INTS, sample_variance=True)
+        result = self.calculate(RANDOM_INTS, sample_variance=True)
+        self.compare_stats(scipy_result, result)
 
+    def test_population_variance_integers(self) -> None:
+        scipy_result = self.calculate_scipy(RANDOM_INTS)
+        result = self.calculate(RANDOM_INTS)
+        self.compare_stats(scipy_result, result)
+
+    def test_aggregate_sample_integers(self) -> None:
+        scipy_result = self.calculate_scipy(RANDOM_INTS, sample_variance=True)
+        print(f'Sample variance: {scipy_result.variance}')
+        result = self.calculate_parallel(RANDOM_INTS, sample_variance=True)
+        self.compare_stats(scipy_result, result)
+
+    def test_aggregate_population_integers(self) -> None:
+        scipy_result = self.calculate_scipy(RANDOM_INTS)
+        print(f'Population variance: {scipy_result.variance}')
+        result = self.calculate_parallel(RANDOM_INTS)
+        self.compare_stats(scipy_result, result)
+
+    def test_sample_variance_floats(self) -> None:
+        scipy_result = self.calculate_scipy(RANDOM_FLOATS, sample_variance=True)
+        result = self.calculate(RANDOM_FLOATS, sample_variance=True)
+        self.compare_stats(scipy_result, result)
+
+    def test_population_variance_floats(self) -> None:
+        scipy_result = self.calculate_scipy(RANDOM_FLOATS)
+        result = self.calculate(RANDOM_FLOATS)
+        self.compare_stats(scipy_result, result)
+
+    def test_aggregate_sample_floats(self) -> None:
+        scipy_result = self.calculate_scipy(RANDOM_FLOATS, sample_variance=True)
+        result = self.calculate_parallel(RANDOM_FLOATS, sample_variance=True)
+        self.compare_stats(scipy_result, result)
+
+    def test_aggregate_population_floats(self) -> None:
+        scipy_result = self.calculate_scipy(RANDOM_FLOATS)
+        result = self.calculate_parallel(RANDOM_FLOATS)
         self.compare_stats(scipy_result, result)
